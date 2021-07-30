837dc892730da6c1c7b286664fe7563d4225d882
cccs-br <benoit.roy@cyber.gc.ca>
2021-07-30 11:56:10 -0400

    Full test suite.

--- src/test/java/IcebergTest.java
+++ src/test/java/IcebergTest.java
@@ -1,10 +1,48 @@
+import static org.apache.spark.sql.functions.col;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class IcebergTest {
 
-    @Test
-    void test() {
-        IcebergSparkUtil.createTable();
-        IcebergSparkUtil.loadTable();
+    @BeforeAll
+    static void setup() {
+        if(!Files.exists(Path.of("warehouse"))) {
+            IcebergTestUtil.createTable();
+            IcebergTestUtil.loadTable();
+        }
     }
+
+    @Test
+    void query_all() {
+        SparkSession spark = IcebergTestUtil.createSession();
+        Dataset<Row> df = spark.sql("select * from iceberg.ipv6");
+        assertEquals(df.count(), 102);
+    }
+
+    @Test
+    void filter_ok() {
+        SparkSession spark = IcebergTestUtil.createSession();
+        Dataset<Row> df = spark.sql("select * from iceberg.ipv6 where" +
+                " ip_bytes > ipv6_to_bytes('6540:cf5b:fed2:100e:71f1:ae06:76d4:c2f')");
+        assertEquals(df.count(), 59);
+    }
+
+    @Test
+    void filter_error() {
+        SparkSession spark = IcebergTestUtil.createSession();
+        byte[] crit = IcebergTestUtil.ipv6ToBytes("6540:cf5b:fed2:100e:71f1:ae06:76d4:c2f");
+        Dataset<Row> df = spark.sql("select * from iceberg.ipv6").where(
+                col("ip_bytes").$greater(crit)
+        );
+        assertEquals(df.count(), 59);
+    }
+
 }
---
+++ src/test/java/IcebergTestUtil.java
@@ -0,0 +1,63 @@
+import com.googlecode.ipv6.IPv6Address;
+import static org.apache.spark.sql.functions.udf;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import static org.apache.spark.sql.functions.col;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.expressions.UserDefinedFunction;
+import org.apache.spark.sql.types.DataTypes;
+
+public class IcebergTestUtil {
+
+    public static byte[] ipv6ToBytes(String ipv6Str) {
+        return IPv6Address.fromString(ipv6Str).toByteArray();
+    }
+
+    public static UserDefinedFunction ipv6ToBytesUdf()  {
+        return udf((UDF1<String, byte[]>) IcebergTestUtil::ipv6ToBytes, DataTypes.BinaryType);
+    }
+
+    public static void register(SparkSession spark) {
+        spark.udf().register("ipv6_to_bytes", ipv6ToBytesUdf());
+    }
+
+
+    public static SparkSession createSession() {
+        SparkSession spark = SparkSession.builder().master("local")
+                .appName("iceberg.test")
+                .config("spark.sql.catalog.iceberg", "org.apache.iceberg.spark.SparkCatalog")
+                .config("spark.sql.catalog.iceberg.type", "hadoop")
+                .config("spark.sql.catalog.iceberg.warehouse", "warehouse")
+                .config("spark.sql.warehouse.dir", "warehouse")
+                .getOrCreate();
+        register(spark);
+        return spark;
+    }
+
+    public static void createTable() {
+        SparkSession spark = createSession();
+        UserDefinedFunction ipv6UDF = ipv6ToBytesUdf();
+        Dataset<Row> df = spark.read().option("header", true)
+                .csv("src/test/resources/ipv6_addresses_header.txt")
+                .withColumn("ip_bytes", ipv6UDF.apply(col("ip_string")));
+        df.printSchema();
+        spark.sql(String.format("CREATE TABLE iceberg.ipv6 (%s)", df.schema().toDDL()));
+    }
+
+    public static void loadTable() {
+        SparkSession spark = createSession();
+        UserDefinedFunction ipv6UDF = ipv6ToBytesUdf();
+        Dataset<Row> df = spark.read().option("header", true)
+                .csv("src/test/resources/ipv6_addresses_header.txt")
+                .withColumn("ip_bytes", ipv6UDF.apply(col("ip_string")));
+        df.write().mode("overwrite").insertInto("iceberg.ipv6");
+    }
+
+    public static void queryTable() {
+        SparkSession spark = createSession();
+        spark.sql("select * from iceberg.ipv6").show();
+    }
+
+}
