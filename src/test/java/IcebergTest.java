b8ca21224cb2af14a2137d897d5c0a763464ed94
cccs-br <benoit.roy@cyber.gc.ca>
2021-07-30 12:16:38 -0400

    Commtting rest of code.

--- src/test/java/IcebergTest.java
+++ src/test/java/IcebergTest.java
@@ -28,15 +28,36 @@ public class IcebergTest {
     }
 
     @Test
-    void filter_ok() {
+    void filter_spark_c1_ok() {
         SparkSession spark = IcebergTestUtil.createSession();
-        Dataset<Row> df = spark.sql("select * from iceberg.ipv6 where" +
-                " ip_bytes > ipv6_to_bytes('6540:cf5b:fed2:100e:71f1:ae06:76d4:c2f')");
+        Dataset<Row> df = spark.read().parquet("warehouse/ipv6/data").where(
+                "ip_bytes > ipv6_to_bytes('6540:cf5b:fed2:100e:71f1:ae06:76d4:c2f')"
+        );
         assertEquals(df.count(), 59);
     }
 
     @Test
-    void filter_error() {
+    void filter_spark_c2_ok() {
+        SparkSession spark = IcebergTestUtil.createSession();
+        byte[] crit = IcebergTestUtil.ipv6ToBytes("6540:cf5b:fed2:100e:71f1:ae06:76d4:c2f");
+        Dataset<Row> df = spark.read().parquet("warehouse/ipv6/data").where(
+                col("ip_bytes").$greater(crit)
+        );
+        assertEquals(df.count(), 59);
+    }
+
+
+    @Test
+    void filter_iceberg_c1_ok() {
+        SparkSession spark = IcebergTestUtil.createSession();
+        Dataset<Row> df = spark.sql("select * from iceberg.ipv6").where(
+                "ip_bytes > ipv6_to_bytes('6540:cf5b:fed2:100e:71f1:ae06:76d4:c2f')"
+        );
+        assertEquals(df.count(), 59);
+    }
+
+    @Test
+    void filter_iceberg_c2_error() {
         SparkSession spark = IcebergTestUtil.createSession();
         byte[] crit = IcebergTestUtil.ipv6ToBytes("6540:cf5b:fed2:100e:71f1:ae06:76d4:c2f");
         Dataset<Row> df = spark.sql("select * from iceberg.ipv6").where(
